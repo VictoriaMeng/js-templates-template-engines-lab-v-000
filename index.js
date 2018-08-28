ac1c184d34b7e202fa573334031c0aba54916877
Victoria Meng <nights1ststar@protonmail.com>
2018-08-28 17:22:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,15 +21,15 @@ function createPost() {
 
 function postComment() {
   const commenter = document.getElementById("commenterName").value;
-const comment = document.getElementById("commentText").value;
+  const comment = document.getElementById("commentText").value;
 
-const commentTemplate = document.getElementById("comment-template").innerHTML;
+  const commentTemplate = document.getElementById("comment-template").innerHTML;
 
-const templateFn = _.template(commentTemplate);
+  const templateFn = _.template(commentTemplate);
 
-let commentsDiv = document.getElementById("comments");
+  let commentsDiv = document.getElementById("comments");
 
-const templateHTML = templateFn({'comment': comment, 'commenter': commenter});
+  const templateHTML = templateFn({'comment': comment, 'commenter': commenter});
 
-commentsDiv.innerHTML += templateHTML;
+  commentsDiv.innerHTML += templateHTML;
 };
