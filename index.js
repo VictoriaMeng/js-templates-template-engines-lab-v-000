0614d1cfa0b3b3710278f1c84541b901ec3c9acd
Victoria Meng <nights1ststar@protonmail.com>
2018-08-28 17:14:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ function createPost() {
   const post = document.getElementById('post-template').innerHTML;
   const comments = document.getElementById('comments-template').innerHTML;
 
+  const pageTemplate = _.template(page);
+  const postTemplate = _.template(post);
+  const commentsTemplate = _.template(comments);
+
+  const postTitle = document.getElementById("postTitle").value;
+  const postAuthor = document.getElementById("postAuthor").value;
+  const postBody = document.getElementById("postBody").value;
+
+  
 
 };
 
