8780c36880186330e63b28178bfbf84c5c379ff4
Victoria Meng <nights1ststar@protonmail.com>
2018-08-28 17:02:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function createPost() {
+  const page = document.getElementById('page-template').innerHTML;
+  const post = document.getElementById('post-template').innerHTML;
+  const comments = document.getElementById('comments-template').innerHTML;
+
 
 };
